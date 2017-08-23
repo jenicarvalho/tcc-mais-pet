3229c28d8259c71a594116810bd92c9257a671e3
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-08-23 20:35:40 -0300

    feat(head footer): criado includes de head.php e footer.php

---
+++ admin/View/includes/head.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Sistema de procriação de animais">
+        <meta name="author" content="Jeniffer Carvalho">
+        <!-- App Favicon -->
+        <link rel="shortcut icon" href="view/assets/images/favicon.ico">
+        <!-- App title -->
+        <title>:: Área Administrativa :: Acesso Restrito</title>
+
+        <link href="view/assets/plugins/summernote/dist/summernote.css" rel="stylesheet" /><!-- Custom box css -->
+        <link href="view/assets/plugins/custombox/dist/custombox.min.css" rel="stylesheet">
+        <!-- App CSS -->
+        <link href="view/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="view/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+        <script src="view/assets/js/modernizr.min.js"></script>
+
+    </head>
+<body class="fixed-left">
