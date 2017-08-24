cd2bed4b03f2ccbd17c662ece2375f8a1260f72c
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-08-23 21:19:44 -0300

    fix(view login): alterado view de login

--- admin/View/Home/login.php
+++ admin/View/Home/login.php
@@ -5,53 +5,54 @@
 ?>
 
 
-    <!-- Begin page -->
- <div id="wrapper">
-
-    <!-- Top Bar Start -->
-    <div class="topbar">
-
-        <!-- LOGO -->
-        <div class="topbar-left" style="width: 100%">
-            <a href="index.html" class="logo"><span>Mais<span>PET</span></span><i class="zmdi zmdi-layers"></i></a>
-        </div>
-    </div>
-
-    <div class="content-page" style="margin: 2em auto 0; width: 500px ">
-        <div class="content">
-            <div class="container">
-                <div class="card-box">
-                    <div class="row">
-                        <div class="col-md-12" style="float: none;">
-                            <h4 class="m-t-0 header-title"><b>Login</b></h4>
-                            <p class="text-muted m-b-30 font-13">
-                               Preencha os dados abaixo para fazer o login
-                            </p>
-
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Email</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="seuemail@conta.com" name="usuario">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Senha</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="******" name="senha">
-                                </div>
-                                <?php if($success == false) : ?>
-
-                                <p class='bg-danger' style="text-align: center; color: #fff" >Dados incorretos</p>
-
-                                <?php endif; ?>                                
-                                <button type="submit" class="btn btn-purple waves-effect waves-light" name="fazerLogin">Enviar</button>
-                            </form>
-                        </div>
-                    </div><!-- end row -->
-                </div>
+        <div class="account-pages"></div>
+        <div class="clearfix"></div>
+        <div class="wrapper-page">
+            <div class="text-center">
+                <a href="index.html" class="logo"><span>Mais<span>Pet</span></span></a>
+                <h5 class="text-muted m-t-0 font-600">: Área Administrativa :</h5>
             </div>
+            <div class="m-t-40 card-box">
+                <div class="text-center">
+                    <h4 class="text-uppercase font-bold m-b-0">Login</h4>
+                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal m-t-20" action="" method="post">
+
+                        <div class="form-group ">
+                            <div class="col-xs-12">
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="seuemail@conta.com" name="usuario">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="******" name="senha">
+
+                                <?php if($success == false) : ?>
+                                <p class='bg-danger' style="text-align: center; color: #fff; margin-top: 10px" >Dados incorretos</p>
+                                <?php endif; ?>  
+                            </div>
+                        </div>
+
+                        <div class="form-group text-center m-t-30">
+                            <div class="col-xs-12">     
+                                <button type="submit" class="btn btn-custom btn-bordred btn-block waves-effect waves-light" name="fazerLogin">Enviar</button>
+                            </div>                           
+                        </div>
+
+                        <div class="form-group m-t-30 m-b-0">
+                            <div class="col-sm-12">
+                                <a href="page-recoverpw.html" class="text-muted"><i class="fa fa-lock m-r-5"></i> Esqueceu a senha?</a>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>            
         </div>
-    </div>
- </div>
-        <!-- END wrapper -->
+        <!-- end wrapper page -->
+
 <?php
     require_once "view/includes/footer.php";
 ?>
