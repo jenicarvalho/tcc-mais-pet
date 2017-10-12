f07d9123ac5569ceca155024fcab1284f64bc30d
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-10-12 15:21:15 -0300

    fix(sidebar): isolado a sidebar

--- View/Home/interna-anuncio.php
+++ View/Home/interna-anuncio.php
@@ -4,6 +4,9 @@
     require_once "Model/Animais.php";
     require_once "Model/Proprietarios.php";
 
+    $sucessDepoimento = false;
+    require_once "Controller/DepoimentoController.php";
+
     $animal = new Animais();
     $proprietario = new Proprietarios();
 
@@ -29,7 +32,7 @@
     endif;
 
     //pronome do proprietario
-	if($dadosProprietario->sexo == "Feminino") :
+	if(utf8_encode($dadosProprietario->sexo) == "Feminino") :
 		$pronome = "Ela é a dona";
 	else :
 		$pronome = "Ele é o dono";
@@ -44,20 +47,18 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-md-6">
-							<h1>Perfil do <?php echo $dadosAnimal->nomeAnimal ?></h1>
+							<h1>Perfil do <?php echo utf8_encode($dadosAnimal->nomeAnimal) ?></h1>
 						</div>
 						<div class="col-md-6">
 							<ul class="breadcrumb">
 								<li><a href="?pagina=index">Home</a></li>
-								<li class="active"><?php echo $dadosAnimal->nomeAnimal ?></li>
+								<li class="active"><?php echo utf8_encode($dadosAnimal->nomeAnimal) ?></li>
 							</ul>
 						</div>
 					</div>
 				</div>
 			</section>
-			<!-- Page Heading / End -->
 
-			<!-- Page Content -->
 			<section class="page-content">
 				<div class="container">
 
@@ -68,29 +69,29 @@
 								<div class="row">
 									<div class="col-md-5">
 										<figure class="alignnone">
-											<img src="uploads/animais/<?php echo $dadosAnimal->fotoAnimal?>" alt="<?php echo $dadosAnimal->nomeAnimal ?>">
+											<img src="uploads/animais/<?php echo utf8_encode($dadosAnimal->fotoAnimal)?>" alt="<?php echo utf8_encode($dadosAnimal->nomeAnimal) ?>">
 										</figure>
 									</div>
 									<div class="col-md-7">
-										<h2 class="name"><?php echo $dadosProprietario->nome?></h2>
-										<i class="tagline"><?php echo $pronome?> do(a) <?php echo $dadosAnimal->nomeAnimal ?></i>
+										<h2 class="name"><?php echo utf8_encode($dadosProprietario->nome)?></h2>
+										<i class="tagline"><?php echo $pronome?> do(a) <?php echo utf8_encode($dadosAnimal->nomeAnimal) ?></i>
 										<ul class="meta">
-											<li><?php echo $dadosAnimal->nomeAnimal ?> é um <?php echo $dadosAnimal->tipo ?></li>
-											<li>de <?php echo $dadosAnimal->data_nascimento ?></li>
-											<li>da raça <?php echo $dadosAnimal->raca ?> </li>
-											<li>com porte <?php echo $dadosAnimal->porte ?> </li>
-											<li>e da cor <?php echo $dadosAnimal->cor ?> </li>
+											<li><?php echo utf8_encode($dadosAnimal->nomeAnimal) ?> é um <?php echo utf8_encode($dadosAnimal->tipo) ?></li>						<li><?php echo utf8_encode($dadosAnimal->sexo) ?></li>
+											<li>de <?php echo utf8_encode($dadosAnimal->data_nascimento) ?></li>
+											<li>da raça <?php echo utf8_encode($dadosAnimal->raca) ?> </li>
+											<li>com porte <?php echo utf8_encode($dadosAnimal->porte) ?> </li>
+											<li>e da cor <?php echo utf8_encode($dadosAnimal->cor) ?> </li>
 										</ul>
 										<ul class="info" id="informacoes-animal" style="display: none;">
 											<li><i class="fa fa-map-marker"></i> 
-												Eles estão no <strong><?php echo $dadosProprietario->bairro?></strong> em 
-												<strong><?php echo $dadosProprietario->cidade?></strong>
+												Eles estão no <strong><?php echo utf8_encode($dadosProprietario->bairro)?></strong> em 
+												<strong><?php echo utf8_encode($dadosProprietario->cidade)?></strong>
 											</li>
 											<li><i class="fa fa-whatsapp"></i> 
-												<?php echo $dadosProprietario->celular?>
+												<?php echo utf8_encode($dadosProprietario->celular)?>
 											</li>
 											<li><i class="fa fa-phone"></i> 
-												<?php echo $dadosProprietario->telefone?>
+												<?php echo utf8_encode($dadosProprietario->telefone)?>
 											</li>
 										</ul>
 
@@ -123,11 +124,38 @@
 								<div class="spacer-lg"></div>
 								
 								<h4>Descrição</h4>
-								<p><?php echo $dadosAnimal->descricao?></p>
+								<p><?php echo utf8_encode($dadosAnimal->descricao)?></p>
+								
+								<?php if ( isset($_SESSION['usuarioCliente']) ) : ?>
 
-								<h4>Deixe seu depoimento</h4>
-								<a href="#" class="btn btn-success">Clique aqui para avaliar o animal</a>
+								<h4>Deixe um comentário sobre o <?php echo utf8_encode($dadosAnimal->nomeAnimal) ?> abaixo!</h4>
+               					 <?php
 
+				                    if($sucessDepoimento == true) {?>
+				                      <div class="alert alert-success alert-dismissable">
+				                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i class="fa fa-times"></i></button>
+				                        <strong>Depoimento Feito!</strong> Obrigado!
+				                      </div>
+				                    <?php
+				                  }
+				                ?>  
+								<div class="comentario-animal">
+									
+									<form action="" method="post">
+										<textarea name="depoimento-texto" id="" cols="30" rows="6" class="form-control" placeholder="Digite aqui o seu comentário"></textarea> <br>
+
+										<input type="submit" class="btn btn-success" value="Adicionar Comentário" name="envia-depoimento">
+
+										<input type="hidden" name="idAnimal" value="<?php echo $codigoAnimal?>">
+										<input type="hidden" name="idProprietario" value="<?php echo $dadosAnimal->idProprietario?>">
+									</form>
+								</div>
+								<?php else : ?>								
+
+								<h4>Deixe um comentário sobre o <?php echo utf8_encode($dadosAnimal->nomeAnimal) ?>! <a href="?pagina=login">Faça o login antes</a></h4>
+				
+								<?php endif ?>
+								
 								<div class="spacer-lg"></div>
 
 
@@ -153,15 +181,15 @@
 									<div class="project-item">
 										<div class="project-item-inner">
 											<figure class="alignnone project-img">
-												<img src="uploads/animais/<?php echo $valor->fotoAnimal?>" alt="<?php echo $valor->nomeAnimal?>" />
+												<img src="uploads/animais/<?php echo utf8_encode($valor->fotoAnimal)?>" alt="<?php echo utf8_encode($valor->nomeAnimal)?>" />
 												<div class="overlay">
-													<a href="?pagina=interna-anuncio&CodAnimal=<?php echo $valor->idAnimal?>" class="dlink"><i class="fa fa-link"></i></a>
-													<a href="uploads/animais/<?php echo $valor->fotoAnimal?>" class="popup-link zoom"><i class="fa fa-search-plus"></i></a>
+													<a href="?pagina=interna-anuncio&CodAnimal=<?php echo utf8_encode($valor->idAnimal)?>" class="dlink"><i class="fa fa-link"></i></a>
+													<a href="uploads/animais/<?php echo utf8_encode($valor->fotoAnimal)?>" class="popup-link zoom"><i class="fa fa-search-plus"></i></a>
 												</div>
 											</figure>
 											<div class="project-desc">
-												<h4 class="title"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo $valor->idAnimal?>"><?php echo $valor->nomeAnimal?></a></h4>
-												<span class="desc"><?php echo $valor->raca?> / <?php echo $valor->sexo?></span>
+												<h4 class="title"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo utf8_encode($valor->idAnimal)?>"><?php echo utf8_encode($valor->nomeAnimal)?></a></h4>
+												<span class="desc"><?php echo utf8_encode($valor->raca)?> / <?php echo utf8_encode($valor->sexo)?></span>
 											</div>
 										</div>
 								   </div>
@@ -169,129 +197,10 @@
 								</div>
 							</div>
 						</div>
-
-
-						<aside class="sidebar col-md-3 col-md-pull-9 col-bordered">
-							
-							<hr class="visible-sm visible-xs lg">
-
-							<!-- Widget :: Categories -->
-							<div class="widget_categories widget widget__sidebar">
-								<h3 class="widget-title">Filtre Sua Busca</h3>
-					              <form action="#" method="POST" role="form">
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control" id="tipo-animal">
-					                      	<option value="">Tipo de animal</option>
-				                          	<option value="Cachorro">Cachorro</option>
-				                          	<option value="Gato">Gato</option>
-									    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Sexo</option>
-				                          <option value="Fêmea">Fêmea</option>
-				                          <option value="Macho">Macho</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Porte</option>
-				                          <option value="Médio">Médio</option>
-				                          <option value="Grande">Grande</option>
-				                          <option value="Pequeno">Pequeno</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Idade</option>
-				                          <option value="1 ano">1 ano</option>
-				                          <option value="2 anos">2 anos</option>
-				                          <option value="3 anos">3 anos</option>
-				                          <option value="4 anos">4 anos</option>
-				                          <option value="5 anos">5 anos</option>
-				                          <option value="6 anos">6 anos</option>
-				                          <option value="7 anos">7 anos</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Cor</option>
-				                          <option value="Branco">Branco</option>
-				                          <option value="Preto">Preto</option>
-				                          <option value="Amarelo">Amarelo</option>
-				                          <option value="Cinza">Cinza</option>
-					                    </select>
-					                  </div>
-					                </div>
-                  					<div class="form-group" id="raca_cachorro">
-					                    <fieldset class="fieldset-job_category">
-					                      <div class="field select-style">
-					                        <select name="animal_raca"  class="form-control" required>
-					                          <option>Raça</option>
-					                          <option value="Akita">Akita</option>
-					                          <option value="Beagle">Beagle</option>
-					                          <option value="Basset">Basset</option>
-					                          <option value="Buldogue">Buldogue</option>
-					                          <option value="Chihuahua">Chihuahua</option>
-					                          <option value="Chow Chow">Chow Chow</option>
-					                          <option value="Dálmata">Dálmata</option>
-					                          <option value="Doberman">Doberman</option>
-					                          <option value="Husky">Husky</option>
-					                          <option value="Labrador">Labrador</option>
-					                          <option value="Maltês">Maltês</option>
-					                          <option value="Pastor Alemão">Pastor Alemão</option>
-					                          <option value="Pug">Pug</option>
-					                          <option value="ShihTzu">ShihTzu</option>
-					                          <option value="YorkShire">YorkShire</option>
-					                        </select>
-					                      </div>
-					                    </fieldset>
-				                  </div>
-				                  <div class="form-group" id="raca_gato" style="display: none;">
-				                    <fieldset class="fieldset-job_category">
-				                      <div class="field select-style">
-				                        <select name="animal_raca"  class="form-control" required>
-				                          <option>Raça</option>
-				                          <option value="Angorá">Angorá</option>
-				                          <option value="Himalaio">Himalaio</option>
-				                          <option value="British Shorthair">British Shorthair</option>
-				                          <option value="Maione Coon">Maione Coon</option>
-				                          <option value="Persa">Persa</option>
-				                          <option value="Ragdoll">Ragdoll</option>
-				                          <option value="Siamês">Siamês</option>
-				                          <option value="Sphynx">Sphynx</option>
-				                        </select>
-				                      </div>
-				                    </fieldset>
-				                  </div>						                
-					                <button type="submit" class="btn btn-success btn-block">Buscar animais!</button>
-					              </form>
-							</div>
-
-
-							<div class="widget_text widget widget__sidebar">
-								<hr class="visible-sm visible-xs lg">
-								<div class="call-to-action centered">
-									<div class="cta-txt">
-										<h2>Precisa de um Veterinário?</h2>
-										<p>Deixe isso com a equipe do Mais Pet.</p>
-									</div>
-									<div class="cta-btn">
-										<a href="?pagina=contato" class="btn btn-success">Contato</a>
-									</div>
-								</div>
-							</div>
-
-						</aside>
+						
+						<?php
+						   require_once "view/includes/sidebar-home.php";
+						?>
 
 					</div>
 
--- View/Home/mostra-animais.php
+++ View/Home/mostra-animais.php
@@ -39,28 +39,28 @@
 
                   			<?php foreach( $animal->findAllCustom("LIMIT 9") as $key => $valor) : ?>
 
-                  			<?php $nomeProprietario = $proprietario->find($valor->idProprietario)?>
+                  			<?php $nomeProprietario = $proprietario->find(utf8_encode($valor->idProprietario))?>
 
 			                  <div class="col-xs-12 col-sm-6 col-md-4" data-animation="fadeInLeft" data-animation-delay="0">
 			                    <div class="job-listing-box">
 			                      <figure class="job-listing-img">
-			                        <a href="?pagina=interna-anuncio&CodAnimal=<?php echo $valor->idAnimal?>"><img src="uploads/animais/<?php echo $valor->fotoAnimal ?>" alt="<?php echo $valor->nomeAnimal ?>"></a>
+			                        <a href="?pagina=interna-anuncio&CodAnimal=<?php echo utf8_encode($valor->idAnimal)?>"><img src="uploads/animais/<?php echo utf8_encode($valor->fotoAnimal) ?>" alt="<?php echo utf8_encode($valor->nomeAnimal) ?>"></a>
 			                      </figure>
 			                      <div class="job-listing-body">
-			                        <h4 class="name"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo $valor->idAnimal?>"><?php echo $valor->nomeAnimal ?></a></h4>
+			                        <h4 class="name"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo utf8_encode($valor->idAnimal)?>"><?php echo utf8_encode($valor->nomeAnimal) ?></a></h4>
 			                        <p>			                        	
-			                        	<?php echo $valor->tipo ?>, 
-			                        	<?php echo $valor->sexo ?>, 
-			                        	<?php echo $valor->data_nascimento ?>, 
-			                        	<?php echo $valor->cor ?>, 
-			                        	<?php echo $valor->porte ?>
+			                        	<?php echo utf8_encode($valor->tipo) ?>, 
+			                        	<?php echo utf8_encode($valor->sexo) ?>, 
+			                        	<?php echo utf8_encode($valor->data_nascimento) ?>, 
+			                        	<?php echo utf8_encode($valor->cor) ?>, 
+			                        	<?php echo utf8_encode($valor->porte) ?>
 			                        	
 			                        </p>
 			                      </div>
 			                      <footer class="job-listing-footer">
 			                        <ul class="meta">
-			                          <li class="category">Dono: <strong><?php echo $nomeProprietario->nome ?></strong></li>
-			                          <li class="date"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo $valor->idAnimal?>">Veja o perfil completo</a></li>
+			                          <li class="category">Dono: <strong><?php echo utf8_encode($nomeProprietario->nome) ?></strong></li>
+			                          <li class="date"><a href="?pagina=interna-anuncio&CodAnimal=<?php echo utf8_encode($valor->idAnimal)?>">Veja o perfil completo</a></li>
 			                        </ul>
 			                      </footer>
 			                    </div>
@@ -70,183 +70,10 @@
 
 			                </div>
 			              </div>
-
-						<aside class="sidebar col-md-3 col-md-pull-9 col-bordered">
-							
-							<hr class="visible-sm visible-xs lg">
-
-							<!-- Widget :: Categories -->
-							<div class="widget_categories widget widget__sidebar">
-								<h3 class="widget-title">Filtre Sua Busca</h3>
-					              <form action="#" method="POST" role="form">
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control" id="tipo-animal">
-					                      	<option value="">Tipo de animal</option>
-				                          	<option value="Cachorro">Cachorro</option>
-				                          	<option value="Gato">Gato</option>
-									    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Sexo</option>
-				                          <option value="Fêmea">Fêmea</option>
-				                          <option value="Macho">Macho</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Porte</option>
-				                          <option value="Médio">Médio</option>
-				                          <option value="Grande">Grande</option>
-				                          <option value="Pequeno">Pequeno</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Idade</option>
-				                          <option value="1 ano">1 ano</option>
-				                          <option value="2 anos">2 anos</option>
-				                          <option value="3 anos">3 anos</option>
-				                          <option value="4 anos">4 anos</option>
-				                          <option value="5 anos">5 anos</option>
-				                          <option value="6 anos">6 anos</option>
-				                          <option value="7 anos">7 anos</option>
-					                    </select>
-					                  </div>
-					                </div>
-					                <div class="form-group">
-					                  <div class="select-style">
-					                    <select class="form-control">
-					                      <option>Cor</option>
-				                          <option value="Branco">Branco</option>
-				                          <option value="Preto">Preto</option>
-				                          <option value="Amarelo">Amarelo</option>
-				                          <option value="Cinza">Cinza</option>
-					                    </select>
-					                  </div>
-					                </div>
-                  					<div class="form-group" id="raca_cachorro">
-					                    <fieldset class="fieldset-job_category">
-					                      <div class="field select-style">
-					                        <select name="animal_raca"  class="form-control" required>
-					                          <option>Raça</option>
-					                          <option value="Akita">Akita</option>
-					                          <option value="Beagle">Beagle</option>
-					                          <option value="Basset">Basset</option>
-					                          <option value="Buldogue">Buldogue</option>
-					                          <option value="Chihuahua">Chihuahua</option>
-					                          <option value="Chow Chow">Chow Chow</option>
-					                          <option value="Dálmata">Dálmata</option>
-					                          <option value="Doberman">Doberman</option>
-					                          <option value="Husky">Husky</option>
-					                          <option value="Labrador">Labrador</option>
-					                          <option value="Maltês">Maltês</option>
-					                          <option value="Pastor Alemão">Pastor Alemão</option>
-					                          <option value="Pug">Pug</option>
-					                          <option value="ShihTzu">ShihTzu</option>
-					                          <option value="YorkShire">YorkShire</option>
-					                        </select>
-					                      </div>
-					                    </fieldset>
-				                  </div>
-				                  <div class="form-group" id="raca_gato" style="display: none;">
-				                    <fieldset class="fieldset-job_category">
-				                      <div class="field select-style">
-				                        <select name="animal_raca"  class="form-control" required>
-				                          <option>Raça</option>
-				                          <option value="Angorá">Angorá</option>
-				                          <option value="Himalaio">Himalaio</option>
-				                          <option value="British Shorthair">British Shorthair</option>
-				                          <option value="Maione Coon">Maione Coon</option>
-				                          <option value="Persa">Persa</option>
-				                          <option value="Ragdoll">Ragdoll</option>
-				                          <option value="Siamês">Siamês</option>
-				                          <option value="Sphynx">Sphynx</option>
-				                        </select>
-				                      </div>
-				                    </fieldset>
-				                  </div>						                
-					                <button type="submit" class="btn btn-success btn-block">Buscar animais!</button>
-					              </form>
-							</div>
-
-
-							<div class="widget_text widget widget__sidebar">
-								<hr class="visible-sm visible-xs lg">
-								<!-- Call to Action - alternative -->
-								<div class="call-to-action centered">
-									<div class="cta-txt">
-										<h2>Precisa de um Veterinário?</h2>
-										<p>Deixe isso com a equipe do Mais Pet.</p>
-									</div>
-									<div class="cta-btn">
-										<a href="?pagina=contato" class="btn btn-primary">Contato</a>
-									</div>
-								</div>
-								<!-- Call to Action - alternative / End -->
-							</div>
-
-							<!-- Widget :: Tabbed -->
-							<div class="tabbed-widget widget widget__sidebar">
-								<h3 class="widget-title">Confira Outros Animais</h3>
-								<div class="widget-content">
-									<div class="tabs">
-										<!-- Nav tabs -->
-										<ul class="nav nav-tabs">
-											<li class="active"><a href="#tab-1" data-toggle="tab">Gatos</a></li>
-											<li><a href="#tab-2" data-toggle="tab">Cachorros</a></li>
-										</ul>
-										<!-- Tab panes -->
-										<div class="tab-content">
-											<div class="tab-pane fade in active" id="tab-1">
-												<ul class="latest-posts-list">
-												<?php foreach( $animal->findAllCustom("WHERE tipo = 'Gato' ") as $key => $valor) :  ?>
-													<li>
-														<figure class="thumbnail">
-															<a href="#">
-																<img src="uploads/animais/<?php echo $valor->fotoAnimal?>" alt="<?php echo $valor->nomeAnimal?>" width="50">
-															</a>
-														</figure>
-														<h5 class="title"><a href="#"><?php echo $valor->nomeAnimal?></a></h5>
-														<small>
-															<?php echo $valor->sexo ?>, 
-								                        	<?php echo $valor->data_nascimento ?>
-								                        </small>
-													</li>		
-												<?php endforeach ?>	
-												</ul>
-											</div>
-											<div class="tab-pane fade" id="tab-2">
-												<ul class="latest-posts-list">
-												<?php foreach( $animal->findAllCustom("WHERE tipo = 'Cachorro' ") as $key => $valor) :  ?>
-													<li>
-														<figure class="thumbnail">
-															<a href="#">
-																<img src="uploads/animais/<?php echo $valor->fotoAnimal?>" alt="<?php echo $valor->nomeAnimal?>" width="50">
-															</a>
-														</figure>
-														<h5 class="title"><a href="#"><?php echo $valor->nomeAnimal?></a></h5>
-														<small>
-															<?php echo $valor->sexo ?>, 
-								                        	<?php echo $valor->data_nascimento ?>
-								                        </small>
-													</li>		
-												<?php endforeach ?>	
-												</ul>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!-- /Widget :: Tabbed -->
-						</aside>
+												
+						<?php
+						   require_once "view/includes/sidebar-home.php";
+						?>
 					</div>
 
 				</div>
