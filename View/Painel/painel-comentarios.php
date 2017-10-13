ed635cebbe577b3c59107b09f224e92ef3e673fa
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-10-13 17:23:23 -0300

    feat(lista comentarios recebidos)

--- View/Painel/painel-comentarios.php
+++ View/Painel/painel-comentarios.php
@@ -1,10 +1,17 @@
 <?php
     require_once "view/includes/header-dashboard.php";
+
     require_once "Model/Comentarios.php";
+    $comentario = new Comentarios();
+
+    require_once "Model/Animais.php";
+    $animal = new Animais();
+
+    require_once "Model/Proprietarios.php";
+    $proprietario = new Proprietarios();
     
     $idcliente = $_SESSION['usuarioCliente']->id;
 
-    $comentario = new Comentarios();
 ?>
 
     <!-- Main -->
@@ -17,12 +24,14 @@
             <div class="content col-md-8 col-md-8 col-md-offset-1 col-md-push-3">
 
               <div class="title-accent">
-                <h3>Comentários dados</h3>
+                <h3>Comentários Recebidos</h3>
               </div>
               <div class="row">
                 
                 <?php 
                     foreach( $comentario->findAllDepoimentos($idcliente) as $key => $valor): 
+                      $animalObj = $animal->findAnimal($valor->idAnimal);
+                      $proprietarioObj = $proprietario->find($valor->idProprietario);
                 ?>                
                 <div class="col-md-12">
                   <hr class="visible-sm visible-xs lg">
@@ -32,11 +41,14 @@
                     </blockquote>
                     <div class="bq-author">
                       <figure class="author-img">
-                        <img src="http://placehold.it/60x60" alt="">
-
+                        <img src="uploads/animais/<?php echo $animalObj->fotoAnimal ?>" alt="?php echo $animalObj->nomeAnimal ?>" width="60">
+                        <br>
+                        <a href="#" class="btn btn-success btn-sm" style="margin: 10px 0;">Aceitar</a>
+                        <br>
+                        <a href="#" class="btn btn-danger btn-sm">Excluir </a>
                       </figure>
-                      <h6>Pedro </h6>
-                      <span class="bq-author-info">Dono do Brutus</span>
+                      <h6><?php echo utf8_encode($proprietarioObj->nome) ?> </h6>
+                      <span class="bq-author-info">Comentou no anúncio do <?php echo $animalObj->nomeAnimal ?> </span>
                     </div>
                   </div>
                 </div>                
