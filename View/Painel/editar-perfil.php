8dbc34a48e4debe53a58112cc8820a8a1defa084
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-10-14 18:24:21 -0300

    fix(alteracao): mostra os dados alterados ao dar submit

--- View/Painel/editar-perfil.php
+++ View/Painel/editar-perfil.php
@@ -2,6 +2,10 @@
     $success = false;
     require_once "view/includes/header-dashboard.php";
     require_once "Controller/ProprietariosController.php";
+
+    include_once "Model/Proprietarios.php";
+  $proprietario = new Proprietarios();
+  $resultadoProprietario = $proprietario->find($idcliente);
 ?>
 
     <!-- Main -->
@@ -32,35 +36,35 @@
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Nome</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="nome" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->nome) ?>"/>
+                    <input type="text" class="form-control" name="nome" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->nome) ?>"/>
                   </div>
                 </fieldset>
 
                 <fieldset>
                   <label>Email <span class="required">*</span></label>
                   <div class="field">
-                    <input type="email" class="form-control" name="email" id="account_email" placeholder="you@yourdomain.com" value="<?php echo utf8_encode($resultado->email) ?>" />
+                    <input type="email" class="form-control" name="email" id="account_email" placeholder="you@yourdomain.com" value="<?php echo utf8_encode($resultadoProprietario->email) ?>" />
                   </div>
                 </fieldset>
                 
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Senha</label>
                   <div class="field">
-                    <input type="password" class="form-control" name="pass" id="pass1" placeholder="" value="<?php echo utf8_encode($resultado->senha) ?>"/>
+                    <input type="password" class="form-control" name="pass" id="pass1" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->senha) ?>"/>
                   </div>
                 </fieldset>
 
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">CPF</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="document" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->cpf) ?>"/>
+                    <input type="text" class="form-control" name="document" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->cpf) ?>"/>
                   </div>
                 </fieldset>
 
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Data de Aniversário</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="birthdayDate" id="job_title" placeholder="10/10/1990" value="<?php echo utf8_encode($resultado->data_nascimento) ?>"/>
+                    <input type="text" class="form-control" name="birthdayDate" id="job_title" placeholder="10/10/1990" value="<?php echo utf8_encode($resultadoProprietario->data_nascimento) ?>"/>
                   </div>
                 </fieldset>
 
@@ -70,7 +74,7 @@
                       <label for="job_type">Sexo</label>
                       <div class="field select-style">
                         <select name="sex" id="job_type" class="form-control">
-                          <option value="<?php echo utf8_encode($resultado->sexo) ?>" selected><?php echo utf8_encode($resultado->sexo) ?></option>
+                          <option value="<?php echo utf8_encode($resultadoProprietario->sexo) ?>" selected><?php echo utf8_encode($resultadoProprietario->sexo) ?></option>
                           <option value="Feminino">Feminino</option>
                           <option value="Masculino">Masculino</option>
                         </select>
@@ -82,7 +86,7 @@
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Endereço</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="street" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->endereco) ?>"/>
+                    <input type="text" class="form-control" name="street" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->endereco) ?>"/>
                   </div>
                 </fieldset>
 
@@ -90,14 +94,14 @@
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Cidade</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="city" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->cidade) ?>"/>
+                    <input type="text" class="form-control" name="city" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->cidade) ?>"/>
                   </div>
                 </fieldset>
 
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Bairro</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="neighborhood" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->bairro) ?>"/>
+                    <input type="text" class="form-control" name="neighborhood" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->bairro) ?>"/>
                   </div>
                 </fieldset>
 
@@ -108,7 +112,7 @@
                       <label for="job_type">Estado</label>
                       <div class="field select-style">
                         <select name="state" id="job_type" class="form-control">
-                          <option value="<?php echo utf8_encode($resultado->estado) ?>" selected><?php echo utf8_encode($resultado->estado) ?></option>
+                          <option value="<?php echo utf8_encode($resultadoProprietario->estado) ?>" selected><?php echo utf8_encode($resultadoProprietario->estado) ?></option>
                         </select>
                       </div>
                     </fieldset>
@@ -119,20 +123,20 @@
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Celular</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="phone" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->celular) ?>"/>
+                    <input type="text" class="form-control" name="phone" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->celular) ?>"/>
                   </div>
                 </fieldset>
                 
                 <fieldset class="fieldset-job_title">
                   <label for="job_title">Telefone</label>
                   <div class="field">
-                    <input type="text" class="form-control" name="phoneHouse" id="job_title" placeholder="" value="<?php echo utf8_encode($resultado->telefone) ?>"/>
+                    <input type="text" class="form-control" name="phoneHouse" id="job_title" placeholder="" value="<?php echo utf8_encode($resultadoProprietario->telefone) ?>"/>
                   </div>
                 </fieldset>
 
                 <div class="spacer"></div>
 
-                <input type="hidden" name="usuario_id" value="<?php echo utf8_encode($resultado->id) ?>">
+                <input type="hidden" name="usuario_id" value="<?php echo utf8_encode($resultadoProprietario->id) ?>">
 
                 <p>
                   <input type="submit" name="atualizar" class="btn btn-primary" value="Alterar &rarr;" />
