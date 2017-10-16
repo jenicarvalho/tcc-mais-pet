b4a4c83265b54883ed9ea89def5ce0f04bda3350
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2017-10-16 21:09:29 -0200

    feat(depoimento): aceitar excluir depoimento

--- Controller/ComentarioController.php
+++ Controller/ComentarioController.php
@@ -16,14 +16,34 @@
   	$idAnimal = utf8_decode($_POST['idAnimal']);
   	$idProprietario = utf8_decode($_POST['idProprietario']);
   	$data = date('Y-m-d');
+    $status = 0;
 
   	$comentario->setData($data);
   	$comentario->setIdAnimal($idAnimal);
   	$comentario->setIdProprietario($idProprietario);
-  	$comentario->setDepoimento($texto);
+    $comentario->setDepoimento($texto);
+  	$comentario->setStatus($status);
 
   	if( $comentario->insert() ) {
       return $successDepoimento = true;
     }
+ }
+
+  //aceita depoimento
+  if(isset($_POST['comentario-status'])) {
+
+    $idComentario = (int)$_POST['idComentario'];
+    
+    if($_POST['aceitar'] == 'sim') :
+
+      $status = 1;
+      $comentario->setStatus($status);
+      if($comentario->updateStatus($idComentario)) { return $aceitaDepoimento = true; }
+
+    endif;
+
+    if($_POST['aceitar'] == 'nao') :
+      if($comentario->delete($idComentario)) { return $excluiDepoimento = true; }
+    endif;
 
   }
